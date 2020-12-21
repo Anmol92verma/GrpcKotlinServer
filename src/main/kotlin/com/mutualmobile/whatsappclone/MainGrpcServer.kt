9ced88c473c9485ba0ec1cba715909ee23bff67f
Anmol Verma <anmol.verma4@gmail.com>
2020-12-21 10:37:22 +0530

    code refactor

--- src/main/kotlin/com/mutualmobile/whatsappclone/MainGrpcServer.kt
+++ src/main/kotlin/com/mutualmobile/whatsappclone/MainGrpcServer.kt
@@ -1,14 +1,22 @@
 package com.mutualmobile.whatsappclone
 
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoCollection
 import com.mutualmobile.whatsappclone.signup.AuthServiceImpl
 import io.grpc.ServerBuilder
+import org.bson.Document
 
 object MainGrpcServer {
   @JvmStatic
   fun main(args: Array<String>) {
+     val databaseName = System.getenv("database")
+     val connectionString = System.getenv("mongodburi")
+     val mongoClient = MongoClients.create(connectionString)
+     val mongoDatabase = mongoClient.getDatabase(databaseName)
+     val mongoCollection: MongoCollection<Document> = mongoDatabase.getCollection("users")
     ServerBuilder
         .forPort(8080)
-        .addService(AuthServiceImpl()).build().apply {
+        .addService(AuthServiceImpl(mongoCollection)).build().apply {
           start()
           awaitTermination()
         }
--- src/main/kotlin/com/mutualmobile/whatsappclone/signup/AuthServiceImpl.kt
+++ src/main/kotlin/com/mutualmobile/whatsappclone/signup/AuthServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.mutualmobile.whatsappclone.signup
 
-import com.mongodb.client.MongoClients
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.model.Filters.eq
 import com.mutualmobile.whatsappclone.models.User
@@ -11,50 +10,52 @@ import kotlinx.coroutines.Dispatchers
 import org.bson.Document
 import java.util.*
 
-class AuthServiceImpl : AuthServiceGrpcKt.AuthServiceCoroutineImplBase(coroutineContext = Dispatchers.IO) {
-  private val databaseName = System.getenv("database")
-  private val connectionString = System.getenv("mongodburi")
-
-  private val mongoClient = MongoClients.create(connectionString)
-  private val mongoDatabase = mongoClient.getDatabase(databaseName)
-
-  private val mongoCollection: MongoCollection<Document> = mongoDatabase.getCollection("users")
+class AuthServiceImpl(private val mongoCollection: MongoCollection<Document>) : AuthServiceGrpcKt.AuthServiceCoroutineImplBase(coroutineContext = Dispatchers.IO) {
 
   override suspend fun verifyOtp(request: AuthVerify): AuthResponse {
-    try {
+    return try {
       val verificationCheck = SmsSender.verifyCode(request.phoneNumber, request.code)
       if (verificationCheck.valid) {
-        return signupResponse(200, "OTP Validated!")
+        signupResponse(200, "OTP Validated!")
+      } else {
+        signupResponse(201, "OTP not Valid!")
       }
     } catch (ex: Exception) {
       print(ex)
+      signupResponse(201, "Otp check Failed ${ex.message}")
     }
-    return signupResponse(201, "Otp check Failed")
   }
 
   override suspend fun authorize(request: AuthRequest): AuthResponse {
     val userWPhone = mongoCollection.find(eq(User.PHONE, request.phoneNumber)).first()?.toAppUser()
 
     userWPhone?.let {
-      try {
-        SmsSender.send(request.phoneNumber)
-        //request for otp verification
-        return signupResponse(200, "Check Phone For OTP")
-      } catch (ex: Exception) {
-        println(ex)
-        //request for otp verification
-        return signupResponse(201, ex.message ?: "Can't send sms otp!")
-      }
-
+      return whenUserAlreadyExists(request)
     } ?: run {
-      return try {
-        createUser(request, mongoCollection)
-        SmsSender.send(request.phoneNumber)
-        signupResponse(200, "Account created successfully! Check Phone For OTP")
-      } catch (ex: Exception) {
-        println(ex)
-        signupResponse(201, "Account can't be created! ${ex.message}")
-      }
+      return whenUserDoesNotExist(request)
+    }
+  }
+
+  private fun whenUserDoesNotExist(request: AuthRequest): AuthResponse {
+    return try {
+      createUser(request, mongoCollection)
+      SmsSender.send(request.phoneNumber)
+      signupResponse(200, "Account created successfully! Check Phone For OTP")
+    } catch (ex: Exception) {
+      println(ex)
+      signupResponse(201, "Account can't be created! ${ex.message}")
+    }
+  }
+
+  private fun whenUserAlreadyExists(request: AuthRequest): AuthResponse {
+    return try {
+      SmsSender.send(request.phoneNumber)
+      //request for otp verification
+      signupResponse(200, "Check Phone For OTP")
+    } catch (ex: Exception) {
+      println(ex)
+      //request for otp verification
+      signupResponse(201, ex.message ?: "Can't send sms otp!")
     }
   }
 
