62fcb4ee716b486bf0536f60a8f1a9e8106e84f6
anmol92verma <anmol.verma4@gmail.com>
2021-02-12 16:09:26 +0530

    code refactor file manager

--- src/main/kotlin/com/mutualmobile/whatsappclone/files/FileManagerServiceImpl.kt
+++ src/main/kotlin/com/mutualmobile/whatsappclone/files/FileManagerServiceImpl.kt
@@ -10,6 +10,41 @@ import java.io.FileOutputStream
 class FileManagerServiceImpl :
     FileTransferServiceGrpcKt.FileTransferServiceCoroutineImplBase(coroutineContext = Dispatchers.IO) {
   override suspend fun uploadFile(requests: Flow<FileUploadType>): FileUploadResponseMessage {
+    val fileDir = getFilesDir()
+    requests.flowOn(Dispatchers.IO).collect {
+      writeBytesToFile(fileDir)
+    }
+    return FileUploadResponseMessage.newBuilder().apply {
+      this.success = true
+    }.build()
+  }
+
+  private fun writeBytesToFile(fileDir: Unit) {
+    var file: File? = null
+    var fos: FileOutputStream? = null
+    try {
+      file = (File(fileDir, it.fileName))
+      if (!file!!.exists()) {
+        println("mk createNewFile")
+        file!!.createNewFile()
+      }
+      if (fos == null) {
+        fos = FileOutputStream(file)
+      }
+
+      fos?.write(it.fileData.toByteArray())
+      println("writing byte ${it.fileData}")
+
+
+    } catch (ex: Exception) {
+      ex.printStackTrace()
+    } finally {
+      fos?.close()
+    }
+    println("written all bytes ${file!!.length()}")
+  }
+
+  private fun getFilesDir() {
     val cwd = System.getProperty("user.dir")
     println("Current working directory : $cwd")
     val fileDir = File(cwd + File.separator + "images")
@@ -17,32 +52,7 @@ class FileManagerServiceImpl :
       println("mk dirs")
       fileDir.mkdirs()
     }
-    var file: File? = null
-    var fos: FileOutputStream? = null
-    requests.flowOn(Dispatchers.IO).collect {
-      try {
-        file = (File(fileDir, it.fileName))
-        if (!file!!.exists()) {
-          println("mk createNewFile")
-          file!!.createNewFile()
-        }
-        if (fos == null) {
-          fos = FileOutputStream(file)
-        }
-
-        fos?.write(it.fileData.toByteArray())
-        println("writing byte ${it.fileData}")
-
-
-      } catch (ex: Exception) {
-        ex.printStackTrace()
-      }
-    }
-    println("written all bytes ${file!!.length()}")
-    fos?.close()
-    return FileUploadResponseMessage.newBuilder().apply {
-      this.success = true
-    }.build()
+    return fileDir
   }
 
   override fun downloadFile(request: FileDownloadRequestMessage): Flow<FileDownloadResponseMessage> {
